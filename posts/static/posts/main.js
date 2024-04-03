e8622c61af3c4cff0656a76bcceea2ea9b189373
Karandeep Sandhu <90730377+MarvelousChester@users.noreply.github.com>
2024-04-03 18:51:49 -0400

    Add Spinner, Add Loading Posts through button

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,54 +1,61 @@
-console.log("Hello world");
-
-const helloWorldBox = document.getElementById('hello-world')
+const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
 const postsBox = document.getElementById('posts-box')
+const endBox = document.getElementById('end-box')
 
-//helloWorldBox.innerHTML= 'Hello <b>World</b>'
+let visible = 3
+const getData = () => {
+    $.ajax({
+        type: "GET",
+        url: `/data/${visible}/`, // when you press on button click, this visible grow, 3 to 6, to 9
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                data.forEach(element => {
+                    postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${element.title}</h5>
+                            <p class="card-text">${element.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>                    
+                        </div>
+                    </div>
 
+                    `
+                });
+            }, 100)
+            console.log(response.size)
+            if(response.size === 0){
+                endBox.textContent = 'No Posts Added Yet...'
+            }
+            else if(response.size <= visible){
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = 'No more posts to load..'
+            }
+        },
+        error: function(error){
+            console.log(error)
+        },
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success:function(response){
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    }, 
-    error: function(error){
-        console.log(error)
-    }
+    })
+}
+
+loadBtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
 
-$.ajax({
-    type: "GET",
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        console.log(data)
-        data.forEach(element => {
-            postsBox.innerHTML += `
-            <div class="card mb-2">
-                <div class="card-body">
-                    <h5 class="card-title">${element.title}</h5>
-                    <p class="card-text">${element.body}</p>
-                </div>
-                <div class="card-footer">
-                    <div class="row">
-                        <div class="col-1">
-                            <a href="#" class="btn btn-primary">Details</a>
-                        </div>
-                        <div class="col-1">
-                            <a href="#" class="btn btn-primary">Like</a>
-                        </div>
-                    </div>                    
-                </div>
-            </div>
-
-            `
-        }, 100);
-    },
-    error: function(error){
-        console.log(error)
-    },
-
-})
+getData()
