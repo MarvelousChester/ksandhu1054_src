9d0d3b540a74f49a6524a806645bb04e96c19c10
Karandeep Sandhu <90730377+MarvelousChester@users.noreply.github.com>
2024-04-03 20:50:28 -0400

    Add Modal Form with Crispy

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
 # Create your views here.
 
 def post_list_and_create(request):
-    qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs}) # Will be using the key in the template
+    form = PostForm(request.POST or None)
+
+    if request == "POST":
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.Author = author
+            instance.save()
+            # Saving Newly Created Object
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'posts/main.html', context) # Will be using the key in the template
 
 # initialy display 3 posts and then increase them, get lower and upper and then add thme
 def load_post_data_view(request, num_posts):
