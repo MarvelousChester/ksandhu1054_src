d85c142f37726c4750bdeef0c9306315846d6ccd
Karandeep Sandhu <90730377+MarvelousChester@users.noreply.github.com>
2024-04-04 14:20:42 -0400

    Add getting details of post

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from .views import(
     post_list_and_create,
     like_unlike_post,
     post_detail,
+    post_detail_data_view,
 )
 
 app_name = 'posts'
@@ -13,4 +14,6 @@ urlpatterns = [
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
 ]
+
